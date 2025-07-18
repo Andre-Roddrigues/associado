5c8b47b597b73691613e3fb969a1b2435bd84114
Andre_Rodrigues_JR <lariotyler6@gmail.com>
2025-07-18 13:25:34 +0200

    feat:delete cursos folder

--- app/inicio/page.tsx
+++ app/inicio/page.tsx
@@ -5,7 +5,6 @@ import { HeroSection } from '@/components/Landing/Hero/heroPage';
 import { Header } from '@/components/Landing/Header/header';
 import { SaqueSection } from '@/components/Landing/Saques/saquesSection';
 import { CoursesSection } from '@/components/Landing/CadastrarCurso/cadastrarcurso';
-import { FAQ } from '@/components/FAQ/FAQ';
 import {PremiumPlatformSection} from '@/components/Landing/info/informacao';
 import {AutoresSection} from '@/components/Home/intrutor/SectionAutores';
 import { isAuthenticated } from '@/lib/auth-utils';
